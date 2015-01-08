6bd16dde3dc67668e41ce47070f4d8de2c1f19e9
icereval <icereval@gmail.com>
2015-01-08 16:36:36 -0700

    more

--- waterbutler/build.sh
+++ waterbutler/build.sh
@@ -1,4 +1,5 @@
 docker build -t waterbutler_data ./data
+docker build -t waterbutler_redis ./redis
 docker build -t waterbutler_rabbitmq ./rabbitmq
-# docker build -t waterbutler_celery ./celery
-# docker build -t waterbutler_tornado ./tornado
+docker build -t waterbutler_celery ./celery
+docker build -t waterbutler_tornado ./tornado
--- waterbutler/run.sh
+++ waterbutler/run.sh
@@ -1,34 +1,24 @@
+# docker run \
+#     -d \
+#     -it \
+#     --name waterbutler_data \
+#     --volume ${PWD}/.data/rabbitmq/log:/data/rabbitmq/log \
+#     --volume ${PWD}/.data/rabbitmq/mnesia:/data/rabbitmq/mnesia \
+#     waterbutler_data
+
 docker run \
     -d \
-    -it \
-    --name waterbutler_data \
-    --volume ${PWD}/.data/log:/data/log \
-    --volume ${PWD}/.data/mnesia:/data/mnesia \
-    waterbutler_data
+    --name waterbutler_redis \
+    --volume ${PWD}/.data/redis/data:/data \
+    waterbutler_redis
 
 docker run \
     -d \
     --name waterbutler_rabbitmq \
-    --volumes-from waterbutler_data \
+    --volume ${PWD}/.data/rabbitmq/log:/data/log \
+    --volume ${PWD}/.data/rabbitmq/mnesia:/data/mnesia \
+    --link waterbutler_redis:redis \
     waterbutler_rabbitmq
-
-# docker run \
-#     -d \
-#     --name waterbutler_redis \
-#     --volumes-from waterbutler_data \
-#     dockerfile/redis
-
-docker run \
-    -d \
-    --name waterbutler_tornado \
-    --env ENV=test \
-    --volume ${HOME}/.cos/waterbutler-test.json:/home/docker/.cos/waterbutler-test.json:ro \
-    --publish 7777:7777 \
-    --volumes-from waterbutler_data \
-    waterbutler_tornado
-
-    # --link waterbutler_rabbitmq:rabbitmq \
-    # --link waterbutler_redis:redis \
 
 # docker run \
 #     -d \
@@ -39,8 +29,12 @@ docker run \
 #     --link waterbutler_redis:redis \
 #     waterbutler_celery
 
-
-# docker run \
-#     --name waterbutler_tornado \
-#     -p 7777:7777 \
-#     waterbutler_tornado
+docker run \
+    -d \
+    --name waterbutler_tornado \
+    --env ENV=test \
+    --volume ${HOME}/.cos/waterbutler-test.json:/home/docker/.cos/waterbutler-test.json:ro \
+    --publish 7777:7777 \
+    --link waterbutler_rabbitmq:rabbitmq \
+    --link waterbutler_redis:redis \
+    waterbutler_tornado
