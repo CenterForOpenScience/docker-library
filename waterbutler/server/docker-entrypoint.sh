b6a5103b120ce97a3df8d791ff055ef04b3d45ed
icereval <icereval@gmail.com>
2015-01-11 19:09:43 -0500

    create home directory

--- waterbutler/server/docker-entrypoint.sh
+++ waterbutler/server/docker-entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 if [[ "$1" = invoke-* ]]; then
     git clone -b $SOURCE_BRANCH $SOURCE_REPO .
-    git reset --hard 04df0857b34fa3f9b14491067c169b7c17132408
     pip install -r requirements.txt
     chown -R python /code
 
