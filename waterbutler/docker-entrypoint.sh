b2f25628da01d5cab7d1049b7f47144272363530
icereval <icereval@gmail.com>
2015-01-13 17:37:47 -0500

    build code in image

--- waterbutler/docker-entrypoint.sh
+++ waterbutler/docker-entrypoint.sh
@@ -4,11 +4,6 @@ set -e
 export HOME=/home/python
 
 if [[ "$1" = invoke-* ]]; then
-    git clone -b $SOURCE_BRANCH $SOURCE_REPO .
-    pip install -U -r requirements.txt
-    python setup.py develop
-    chown -R python /code
-
     if [ "$1" = 'invoke-celery' ]; then
         echo "Starting Celery"
         exec gosu python bash -c "invoke celery"
