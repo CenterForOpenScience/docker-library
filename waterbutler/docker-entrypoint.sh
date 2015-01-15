5f4981c815cd208e1a47d0beeae3deeb35f1626f
icereval <icereval@gmail.com>
2015-01-15 09:29:39 -0500

    only chown python osfstorage if necessary, run full docker command

--- waterbutler/docker-entrypoint.sh
+++ waterbutler/docker-entrypoint.sh
@@ -3,24 +3,17 @@ set -e
 
 export HOME=/home/python
 
-chown -R python /data/osfstorage/pending
-chown -R python /data/osfstorage/complete
+if [[ $(stat -c '%U' /data/osfstorage/pending) != python ]]; then
+    chown -R python /data/osfstorage/pending
+fi
+
+if [[ $(stat -c '%U' /data/osfstorage/complete) != python ]]; then
+    chown -R python /data/osfstorage/complete
+fi
 
 git pull
 pip install -U -r requirements.txt
 python setup.py develop
 chown -R python /code
-
-if [[ "$1" = invoke-* ]]; then
-    if [ "$1" = 'invoke-celery' ]; then
-        echo "Starting Celery"
-        exec gosu python bash -c "invoke celery"
-    fi
-
-    if [ "$1" = 'invoke-server' ]; then
-        echo "Starting Server"
-        exec gosu python bash -c "invoke server"
-    fi
-fi
 
 exec gosu python bash -c "$@"
