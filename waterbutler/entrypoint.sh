9c1f4debc9934c783dc7de6670f66b4053393ef5
icereval <icereval@gmail.com>
2015-01-19 14:24:46 -0500

    chmod/own ~/.cos in docker waterbutler config

--- waterbutler/entrypoint.sh
+++ waterbutler/entrypoint.sh
@@ -11,6 +11,9 @@ if [[ $(stat -c '%U' /data/osfstorage/complete) != python ]]; then
     chown -R python /data/osfstorage/complete
 fi
 
+chmod -R 0770 ~/.cos
+chown -R python ~/.cos
+
 git pull
 pip install -U -r requirements.txt
 python setup.py develop
