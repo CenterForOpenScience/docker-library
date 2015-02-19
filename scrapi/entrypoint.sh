5e8edd53d650f7b57a892afdb1536e451c8fc4a5
icereval <icereval@gmail.com>
2015-02-19 12:28:17 -0500

    settings symbolic link

--- scrapi/entrypoint.sh
+++ scrapi/entrypoint.sh
@@ -11,6 +11,9 @@ if [[ $(stat -c '%U' /code) != python ]]; then
     git clone -b $SOURCE_BRANCH $SOURCE_REPO .
 fi
 
+chown -R python ~/.cos
+ln -s ~/.cos/local.py /code/settings/local.py
+
 git pull
 pip install -U -r requirements.txt
 chown -R python /code
