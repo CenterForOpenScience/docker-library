dc6a04b8e4f02b81486fd2ae1c99637b8fe789a8
icereval <icereval@gmail.com>
2015-02-18 23:50:15 -0500

    scrapi

---
+++ scrapi/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+export HOME=/home/python
+
+if [[ $(stat -c '%U' /data) != python ]]; then
+    chown python /data
+fi
+
+if [[ $(stat -c '%U' /log) != python ]]; then
+    chown python /log
+fi
+
+if [[ $(stat -c '%U' /code) != python ]]; then
+    git clone -b $SOURCE_BRANCH $SOURCE_REPO .
+fi
+
+git pull
+pip install -U -r requirements.txt
+chown -R python /code
+
+exec gosu python "$@"
