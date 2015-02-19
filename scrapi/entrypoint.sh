0aa25dc6996f048d88fc5872e7a464b3651d1f0b
icereval <icereval@gmail.com>
2015-02-19 14:37:46 -0500

    link settings for scrapi

--- scrapi/entrypoint.sh
+++ scrapi/entrypoint.sh
@@ -7,12 +7,12 @@ if [[ $(stat -c '%U' /log) != python ]]; then
     chown python /log
 fi
 
+chown -R python ~/.cos
+
 if [[ $(stat -c '%U' /code) != python ]]; then
     git clone -b $SOURCE_BRANCH $SOURCE_REPO .
+    ln -s ~/.cos/local.py /code/scrapi/settings/local.py
 fi
-
-chown -R python ~/.cos
-ln -s ~/.cos/local.py /code/scrapi/settings/local.py
 
 git pull
 pip install -U -r requirements.txt
