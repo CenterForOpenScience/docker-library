a9b416ac70deac8f12498626588d63d78eda1bc7
icereval <icereval@gmail.com>
2015-03-26 12:17:27 -0400

    share registration

---
+++ share/registration/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+export HOME=/home/python
+
+if [[ $(stat -c '%U' /log) != python ]]; then
+    chown python /log
+fi
+
+chown -R python ~/.cos
+
+if [[ $(stat -c '%U' /code) != python ]]; then
+    git clone -b $SOURCE_BRANCH $SOURCE_REPO .
+    ln -s ~/.cos/local.py /code/scrapi/settings/local.py
+    chown -R python /code
+fi
+
+git pull
+pip install -U -r requirements.txt
+chown -R python /code
+
+exec gosu python "$@"
