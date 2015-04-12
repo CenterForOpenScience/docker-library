ed20cbaf3b87491e9236f22f604c5a2dee7e6747
icereval <icereval@gmail.com>
2015-04-12 11:35:00 -0400

    cas

--- cas/entrypoint.sh
+++ cas/entrypoint.sh
@@ -6,9 +6,7 @@ export HOME=/home/cas
 if [[ $(stat -c '%U' /cas-overlay) != cas ]]; then
     git clone -b $SOURCE_BRANCH $SOURCE_REPO .
     # ln -s ~/.cos/local.py /cas-overlay/settings/local.py
-    mvn clean
-    mvn install
-    mvn package
+    mvn clean install
 fi
 
 chown -R cas ~/.cos
