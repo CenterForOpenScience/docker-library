c2d6519bf9228436d8dc6de21e5a0eaafd25711a
icereval <icereval@gmail.com>
2015-06-15 13:46:37 -0400

    unoconv log dir

--- unoconv/entrypoint.sh
+++ unoconv/entrypoint.sh
@@ -7,4 +7,8 @@ if [[ $(stat -c '%U' /unoconv) != unoconv ]]; then
     chown unoconv /unoconv
 fi
 
+if [[ $(stat -c '%U' /log) != unoconv ]]; then
+    chown unoconv /log
+fi
+
 exec gosu unoconv "$@"
