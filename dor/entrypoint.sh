e16ae7cd47d35649a7cca80ff08e7f6591193037
icereval <icereval@gmail.com>
2015-06-17 18:53:16 -0400

    dor, uwsgi

--- dor/entrypoint.sh
+++ dor/entrypoint.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 set -e
 
-export HOME=/home/python
-chown -R python ~/
+chown -R www-data /var/www
 
-if [[ $(stat -c '%U' /log) != python ]]; then
-    chown python /log
+if [[ $(stat -c '%U' /log) != www-data ]]; then
+    chown www-data /log
 fi
 
-if [[ $(stat -c '%U' /code) != python ]]; then
-    chown -R python /code
-    gosu python git clone -b $SOURCE_BRANCH $SOURCE_REPO .
-    gosu python ln -s ~/.cos/local.py /code/RepoDir/settings/local.py
+if [[ $(stat -c '%U' /code) != www-data ]]; then
+    chown -R www-data /code
+    gosu www-data git clone -b $SOURCE_BRANCH $SOURCE_REPO .
+    gosu www-data ln -s /var/www/.cos/local.py /code/RepoDir/settings/local.py
 fi
 
-gosu python git pull
+gosu www-data git pull
 pip install -U -r requirements.txt
-gosu python bower install --config.interactive=false
-gosu python python manage.py collectstatic --noinput
-
-# ningx runas as user nginx, not the first docker user
-touch /tmp/uwsgi.sock
-chmod o+w /tmp/uwsgi.sock
+gosu www-data bower install --config.interactive=false
+gosu www-data python manage.py collectstatic --noinput
 
 exec gosu root "$@"
