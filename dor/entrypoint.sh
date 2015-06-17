c0a52e971df8b0856581818d106251a280125d38
icereval <icereval@gmail.com>
2015-06-17 19:19:11 -0400

    dor

--- dor/entrypoint.sh
+++ dor/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-chown -R www-data /var/www
+chown -R www-data:www-data /home/.cos
 
 if [[ $(stat -c '%U' /log) != www-data ]]; then
-    chown www-data /log
+    chown www-data:www-data /log
 fi
 
 if [[ $(stat -c '%U' /code) != www-data ]]; then
-    chown -R www-data /code
+    chown -R www-data:www-data /code
     gosu www-data git clone -b $SOURCE_BRANCH $SOURCE_REPO .
     gosu www-data ln -s /var/www/.cos/local.py /code/RepoDir/settings/local.py
 fi
