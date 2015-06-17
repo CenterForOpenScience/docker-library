3ef6caf98eb8e05eb9cd5cc1091a1594ed313066
icereval <icereval@gmail.com>
2015-06-17 10:49:29 -0400

    dor

---
+++ dor/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+export HOME=/home/python
+chown -R python ~/
+
+if [[ $(stat -c '%U' /log) != python ]]; then
+    chown python /log
+fi
+
+if [[ $(stat -c '%U' /code) != python ]]; then
+    chown python /code
+    gosu python git clone -b $SOURCE_BRANCH $SOURCE_REPO .
+    gosu python ln -s ~/.cos/local.py /code/RepoDir/settings/local.py
+fi
+
+gosu python git pull
+pip install -U -r requirements.txt
+gosu python bower install --config.interactive=false
+gosu python python manage.py collectstatic --noinput
+
+exec gosu root "$@"
