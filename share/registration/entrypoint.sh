876b45233aeb927f3ad71f3fe7fe2e3aac7770f5
icereval <icereval@gmail.com>
2015-06-18 17:13:47 -0400

    share registration w/ new uwsgi build

--- share/registration/entrypoint.sh
+++ share/registration/entrypoint.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 set -e
 
-export HOME=/home/python
+chown -R www-data:www-data /home
 
-if [[ $(stat -c '%U' /log) != python ]]; then
-    chown python /log
+if [[ $(stat -c '%U' /log) != www-data ]]; then
+    chown www-data:www-data /log
 fi
 
-chown -R python ~/.cos
-
-if [[ $(stat -c '%U' /code) != python ]]; then
-    git clone -b $SOURCE_BRANCH $SOURCE_REPO .
-    ln -s ~/.cos/local.py /code/shareregistration/settings/local.py
-    chown -R python /code
+if [[ $(stat -c '%U' /code) != www-data ]]; then
+    chown -R www-data:www-data /code
+    gosu www-data git clone -b $SOURCE_BRANCH $SOURCE_REPO .
+    gosu www-data ln -s /home/.cos/local.py /code/shareregistration/settings/local.py
 fi
 
-git pull
+gosu www-data git pull
 pip install -U -r requirements.txt
-npm install --production
-bower install --allow-root --config.interactive=false
-python manage.py collectstatic --noinput
-chown -R python /code
+gosu www-data npm install --production
+gosu www-data bower install --config.interactive=false
+gosu www-data python manage.py collectstatic --noinput
 
 exec gosu root "$@"
