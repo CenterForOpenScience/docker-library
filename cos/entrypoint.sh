df71e0445853f0e93d959eaadd64add5e9a0296c
icereval <icereval@gmail.com>
2015-06-28 23:34:42 -0400

    cos

---
+++ cos/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+chown -R www-data:www-data /home
+
+if [[ $(stat -c '%U' /log) != www-data ]]; then
+    chown www-data:www-data /log
+fi
+
+if [[ $(stat -c '%U' /code) != www-data ]]; then
+    chown -R www-data:www-data /code
+    gosu www-data git clone -b $SOURCE_BRANCH $SOURCE_REPO .
+    gosu www-data ln -s /home/.cos/local.py /code/mysite/local_settings.py
+fi
+
+gosu www-data git pull
+pip install -U -r requirements.txt
+# gosu www-data npm install --production
+gosu www-data bower install --config.interactive=false
+gosu www-data python manage.py collectstatic --noinput
+
+exec gosu root "$@"
