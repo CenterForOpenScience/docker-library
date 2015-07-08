a0f9a9fdff257f7d91a661f6bc619afa3ec0e8a8
icereval <icereval@gmail.com>
2015-07-08 10:13:03 -0400

    tokumx 2.0.1, notes detailing transparent huge page settings

--- tokumx/entrypoint.sh
+++ tokumx/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 
-# Disable huge pages (requires privileged container)
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
-
-if [ "$1" = 'mongod' ]; then
+if [ "$1" = "mongod" ]; then
     chown -R tokumx /data/db
 fi
 
