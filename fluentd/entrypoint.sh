6c60235d3f7c979565c35c14a48eb722ff78e8c1
icereval <icereval@gmail.com>
2015-07-09 17:43:14 -0400

    fluentd log directory

--- fluentd/entrypoint.sh
+++ fluentd/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-if [[ $(stat -c '%U' /var/log) != fluentd ]]; then
-    chown fluentd:fluentd /var/log
+if [[ $(stat -c '%U' /log) != fluentd ]]; then
+    chown fluentd:fluentd /log
 fi
 
 if [ "$FLUENTD_GEMS" != "" ]; then
