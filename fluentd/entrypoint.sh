a90bb885851ba62a31d3aab17c3903e87b5ddc1f
icereval <icereval@gmail.com>
2015-07-09 17:50:46 -0400

    fluetnd as root to access logs from other apps

--- fluentd/entrypoint.sh
+++ fluentd/entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
 
-if [[ $(stat -c '%U' /log) != fluentd ]]; then
-    chown fluentd:fluentd /log
-fi
-
 if [ "$FLUENTD_GEMS" != "" ]; then
     fluent-gem install $FLUENTD_GEMS
 fi
 
-exec gosu fluentd "$@"
+exec root fluentd "$@"
