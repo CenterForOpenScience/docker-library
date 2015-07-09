1124edbcc4270503dc09655bd36b82048480de6f
icereval <icereval@gmail.com>
2015-07-09 17:39:20 -0400

    fluentd log directory

--- fluentd/entrypoint.sh
+++ fluentd/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [[ $(stat -c '%U' /var/log) != fluentd ]]; then
+    chown fluentd:fluentd /var/log
+fi
+
 if [ "$FLUENTD_GEMS" != "" ]; then
     fluent-gem install $FLUENTD_GEMS
 fi
