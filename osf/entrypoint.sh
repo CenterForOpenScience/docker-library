cf4a1d23fe1168eafad65eb48d84c4c23f5f385f
icereval <icereval@gmail.com>
2015-08-03 15:43:12 -0400

    osf

--- osf/entrypoint.sh
+++ osf/entrypoint.sh
@@ -5,11 +5,14 @@ export HOME=/home
 
 if [[ $(stat -c '%U' /code) != www-data ]]; then
     chown -R www-data:www-data /code
-    gosu www-data git clone -b $SOURCE_BRANCH $SOURCE_REPO .
+    gosu www-data git remote rm origin
+    gosu www-data git remote add origin $SOURCE_REPO
+    gosu www-data git fetch -q
+    gosu www-data git checkout $SOURCE_BRANCH
 fi
 
 gosu www-data git pull
 invoke requirements --release
-invoke assets
+gosu www-data invoke assets
 
 exec gosu www-data "$@"
