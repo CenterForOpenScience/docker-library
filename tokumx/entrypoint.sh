ae3f18483cf44905831e63c8a7b2eff986138d17
icereval <icereval@gmail.com>
2015-08-03 16:05:52 -0400

    tokumx

--- tokumx/entrypoint.sh
+++ tokumx/entrypoint.sh
@@ -6,14 +6,14 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 if [ "$1" = 'mongod' ]; then
-	chown -R mongodb /data/db
+	chown -R tokumx /data/db
 
 	numa='numactl --interleave=all'
 	if $numa true &> /dev/null; then
 		set -- $numa "$@"
 	fi
 
-	exec gosu mongodb "$@"
+	exec gosu tokumx "$@"
 fi
 
 exec "$@"
