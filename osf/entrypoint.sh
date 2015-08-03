cd6ebf4285616ae45e37e1d7fb021e5714dec241
icereval <icereval@gmail.com>
2015-08-03 17:11:16 -0400

    osf

--- osf/entrypoint.sh
+++ osf/entrypoint.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 set -e
 
-export HOME=/home
+chown -R www-data:www-data /home
 
 if [[ $(stat -c '%U' /code) != www-data ]]; then
     chown -R www-data:www-data /code
-    gosu www-data git remote rm origin
-    gosu www-data git remote add origin $SOURCE_REPO
-    gosu www-data git fetch -q
-    gosu www-data git checkout $SOURCE_BRANCH
 fi
 
-gosu www-data git pull
+gosu www-data git init
+gosu www-data git remote rm origin
+gosu www-data git remote add origin $SOURCE_REPO
+gosu www-data git fetch -q
+gosu www-data git checkout $SOURCE_BRANCH
+gosu www-data git pull origin $SOURCE_BRANCH
+
 invoke requirements --release
 gosu www-data invoke assets
 
-exec gosu www-data "$@"
+if [ "$1" = 'invoke' ]; then
+    exec gosu www-data "$@"
+fi
+
+exec gosu root "$@"
