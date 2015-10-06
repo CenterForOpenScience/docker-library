89ca64ed6711396c61a2a7cb18fba102ac39f611
icereval <icereval@gmail.com>
2015-10-06 11:10:25 -0400

    waterbutler/mfr

--- mfr/entrypoint.sh
+++ mfr/entrypoint.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 set -e
 
-chown -R www-data:www-data /home
+chown -R www-data:www-data /home || true
+chown -R www-data:www-data /code || true
 
-if [[ $(stat -c '%U' /code) != www-data ]]; then
-    chown -R www-data:www-data /code
-    gosu www-data git clone -b $SOURCE_BRANCH $SOURCE_REPO .
+if [ ! -d /code/.git ]; then
+    gosu www-data git init
 fi
 
-gosu www-data git pull
-pip install -U -r requirements.txt
-python setup.py develop
+gosu www-data git remote rm origin || true
+gosu www-data git remote add origin $SOURCE_REPO
+gosu www-data git fetch -q
+gosu www-data git checkout $SOURCE_BRANCH
+gosu www-data git pull origin $SOURCE_BRANCH
 
-exec gosu www-data "$@"
+# avoid running setup tasks on container restarts
+commit_head=$(git rev-parse HEAD)
+updated=false
+if [ -f "/tmp/.commit" ]; then
+    if ! grep -Fxq "$commit_head" /tmp/.commit; then
+        updated=true
+    fi
+else
+    updated=true
+fi
+if $updated; then
+    if [ "$UPDATE_CMD" != "" ]; then
+        echo "Updating: $UPDATE_CMD"
+        eval $UPDATE_CMD
+    fi
+fi
+echo "$commit_head" > /tmp/.commit
+
+if [ "$1" = 'invoke' ]; then
+    echo "Starting: $@"
+    exec gosu www-data "$@"
+fi
+
+exec gosu root "$@"
