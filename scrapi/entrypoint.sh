a784a41de1edd157f6efd2b358e13b6775f9af76
Fabian von Feilitzsch <fabian@fabianism.us>
2015-10-12 13:11:35 -0400

    link scrapi api settings file

--- scrapi/entrypoint.sh
+++ scrapi/entrypoint.sh
@@ -7,6 +7,7 @@ if [[ $(stat -c '%U' /code) != www-data ]]; then
     chown -R www-data:www-data /code
     gosu www-data git clone -b $SOURCE_BRANCH $SOURCE_REPO .
     gosu www-data ln -s /home/.cos/local.py /code/scrapi/settings/local.py
+    gosu www-data ln -s /home/.cos/local.py /code/api/api/settings/local.py
 fi
 
 gosu www-data git pull
