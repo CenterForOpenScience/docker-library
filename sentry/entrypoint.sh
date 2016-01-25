cdbca265e3c13319af5e4c5e4de56848d96aaf52
icereval <icereval@gmail.com>
2016-01-24 22:04:43 -0500

    sentry

--- sentry/entrypoint.sh
+++ sentry/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-# chown -R www-data:www-data /home || true
-# chown -R www-data:www-data /code || true
+case "$1" in
+	celery|cleanup|config|createuser|devserver|django|export|help|import|init|plugins|repair|shell|start|upgrade)
+		set -- gosu www-data /docker-entrypoint.sh "$@"
+	;;
+esac
 
-exec gosu www-data /docker-entrypoint.sh "$@"
+exec "$@"
