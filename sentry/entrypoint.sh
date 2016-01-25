acc74368db849f0e776095ff8539d8663bae56b5
icereval <icereval@gmail.com>
2016-01-24 23:25:47 -0500

    sentry

--- sentry/entrypoint.sh
+++ sentry/entrypoint.sh
@@ -3,6 +3,12 @@ set -e
 
 chown -R www-data:www-data /var/lib/sentry/files || true
 
+# first check if we're passing flags, if so
+# prepend with sentry
+if [ "${1:0:1}" = '-' ]; then
+	set -- gosu www-data /docker-entrypoint.sh "$@"
+fi
+
 case "$1" in
 	celery|cleanup|config|createuser|devserver|django|export|help|import|init|plugins|repair|shell|start|upgrade)
 		set -- gosu www-data /docker-entrypoint.sh "$@"
