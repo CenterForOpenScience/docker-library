32a5cc3dd5b9aaad39030e9a8c699716283e062b
icereval <icereval@gmail.com>
2016-01-24 22:13:03 -0500

    sentry

--- sentry/entrypoint.sh
+++ sentry/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+chown -R sentry:sentry /var/lib/sentry/files || true
+
 case "$1" in
 	celery|cleanup|config|createuser|devserver|django|export|help|import|init|plugins|repair|shell|start|upgrade)
-		set -- gosu www-data /docker-entrypoint.sh "$@"
+		set -- gosu sentry /docker-entrypoint.sh "$@"
 	;;
 esac
 
