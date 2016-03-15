168de3662e865ff353342a0f0611be65268700ea
icereval <icereval@gmail.com>
2016-03-15 14:34:47 -0400

    ember

---
+++ ember/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+chown -R www-data:www-data /home || true
+chown -R www-data:www-data $WORKDIR || true
+
+if [ ! -d $WORKDIR/.git ]; then
+    gosu www-data git init
+fi
+
+gosu www-data git remote rm origin || true
+gosu www-data git remote add origin $SOURCE_REPO
+gosu www-data git fetch -q
+gosu www-data git checkout $SOURCE_BRANCH
+gosu www-data git pull origin $SOURCE_BRANCH
+
+# avoid running setup tasks on container restarts
+commit_head=$(git rev-parse HEAD)
+updated=false
+if [ -f "/tmp/.commit" ]; then
+    if ! grep -Fxq "$commit_head" /tmp/.commit; then
+        updated=true
+    fi
+else
+    updated=true
+fi
+if $updated; then
+    if [ "$UPDATE_CMD" != "" ]; then
+        echo "Updating: $UPDATE_CMD"
+        eval $UPDATE_CMD
+    fi
+fi
+echo "$commit_head" > /tmp/.commit
+
+if [ "$1" = 'invoke' ]; then
+    echo "Starting: $@"
+    exec gosu www-data "$@"
+fi
+
+exec gosu root "$@"
