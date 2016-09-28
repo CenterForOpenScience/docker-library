1b32736a81609a79cac68892d1de634efc15d242
mattclark <mattclark@users.noreply.github.com>
2016-09-28 17:21:39 -0400

    Updating sentry version.

--- sentry/docker-entrypoint.sh
+++ sentry/docker-entrypoint.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-set -e
 
-chown -R www-data:www-data /var/lib/sentry/files || true
+set -e
 
 # first check if we're passing flags, if so
 # prepend with sentry
 if [ "${1:0:1}" = '-' ]; then
-	set -- gosu www-data /docker-entrypoint.sh "$@"
+	set -- sentry "$@"
 fi
 
 case "$1" in
-	celery|cleanup|config|createuser|devserver|django|export|help|import|init|plugins|repair|shell|start|upgrade)
-		set -- gosu www-data /docker-entrypoint.sh "$@"
+	celery|cleanup|config|createuser|devserver|django|exec|export|help|import|init|plugins|queues|repair|run|shell|start|tsdb|upgrade)
+		set -- sentry "$@"
 	;;
 esac
+
+if [ "$1" = 'sentry' ]; then
+	set -- tini -- "$@"
+	if [ "$(id -u)" = '0' ]; then
+		mkdir -p "$SENTRY_FILESTORE_DIR"
+		chown -R sentry "$SENTRY_FILESTORE_DIR"
+		set -- gosu sentry "$@"
+	fi
+fi
 
 exec "$@"
